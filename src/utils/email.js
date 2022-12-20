2e3cc485446a0677f303548fea452136652f99e9
= <eyosiyasgirma0@gmail.com>
2022-12-20 05:20:11 +0300

    from new device

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,59 +1,31 @@
 const config = require("../../config");
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-
-const createTransport = async() => {
-  const playGround = "https://developers.google.com/oauthplayground";
-  const OAuth2 = google.auth.OAuth2;
-
-  const oauthClient = new OAuth2(
-    config.oAuthClientId,
-    config.oAuthClientSecret,
-    playGround
-  );
-
-  oauthClient.setCredentials({
-    refresh_token: config.oAuthRefreshToken,
-  });
-
-  const accessToken = await new Promise((resolve, reject) => {
-    oauthClient.getAccessToken((err, token) => {
-      if (err) {
-        reject("Failed to create access token :(");
-      }
-      resolve(token);
-    });
-  });
-  const transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: config.mailerEmail,
-      clientId: config.oAuthClientId,
-      clientSecret: config.oAuthClientSecret,
-      refreshToken: config.oAuthRefreshToken,
-      accessToken: accessToken,
-    },
-  });
-
-  return transport;
-};
 
 const sendEmail = async ({ emailTo, subject, code, image, ticket }) => {
-  const transporter = await createTransport();
-  await transporter.sendMail({
-    from: config.mailerEmail,
-    to: emailTo,
-    subject: subject,
-    text: `Dear customer, your payment is finished. Your ticket code is ${code}`,
-    html: ticket,
-    attachments: [
-      {
-        filename: "ticket.png",
-        content: image,
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
       },
-    ],
-  });
+    });
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: emailTo,
+      subject: subject,
+      text: `Dear customer, your payment is finished. Your ticket code is ${code}`,
+      html: ticket,
+      attachments: [
+        {
+          filename: "ticket.png",
+          content: image,
+        },
+      ],  
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 module.exports = sendEmail;
